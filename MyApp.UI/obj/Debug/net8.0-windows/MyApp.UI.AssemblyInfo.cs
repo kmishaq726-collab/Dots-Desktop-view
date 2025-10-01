c7ae5ceb8b5083818de1a22402f51efbab57c389
Muhammad Farhan <Muhammad Farhan>
2025-10-01 19:39:32 +0500

    build again

--- MyApp.UI/obj/Debug/net8.0-windows/MyApp.UI.AssemblyInfo.cs
+++ MyApp.UI/obj/Debug/net8.0-windows/MyApp.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa1d9402dca0df045b07ed5c30692195f33fefb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed0b44c79c0cab6891d334a7f832ca358d363ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
